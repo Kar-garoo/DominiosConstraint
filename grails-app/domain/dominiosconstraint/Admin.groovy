d2c204e09429da973a09baa1b14185527ffddd34
Sergio Alejandro Diaz Pinilla <seradiazpin@unal.edu.co>
2015-04-11 11:11:21 -0500

    Admin domain class

---
+++ grails-app/domain/dominiosconstraint/Admin.groovy
@@ -0,0 +1,14 @@
+package dominiosconstraint
+
+class Admin extends User{
+
+    Integer level
+    Double rating
+
+
+    static constraints = {
+
+        level(min: 1,max: 5,nullable: false)
+        raiting(min: 0,max: 100,nullable: false)
+    }
+}
