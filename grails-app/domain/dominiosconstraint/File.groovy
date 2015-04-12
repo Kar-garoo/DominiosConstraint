3aab73d08c0fd0589d43356fc237f966d8fd5cf7
Miguel <miatorresch@unal.edu.co>
2015-04-12 13:49:49 -0500

    -Ahora si se agregan las Clases File y User

---
+++ grails-app/domain/dominiosconstraint/File.groovy
@@ -0,0 +1,13 @@
+package dominiosconstraint
+
+class File {
+    String fileType
+    Byte[] content
+    Double size
+
+    static constraints = {
+        fileType(matches: ".+[/]+.+")
+        content()
+        size(range:0..10)
+    }
+}
