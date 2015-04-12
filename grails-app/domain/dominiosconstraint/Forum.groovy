1654045fe5be2a894a3599cdf732ccb21c6bd5cf
Daniel <jodsalazarmo@unal.edu.co>
2015-04-12 11:54:09 -0500

    Finalizada, Admin, Forum y Regular sin errores

--- grails-app/domain/dominiosconstraint/Forum.groovy
+++ grails-app/domain/dominiosconstraint/Forum.groovy
@@ -3,12 +3,12 @@ package dominiosconstraint
 class Forum {
 
     String name
-    Date dateCreated
+    Date datecreated
     String category
 
     static constraints = {
         name(minSize: 3, maxSize: 20, unique: true, nullable: false)
         category(minSize: 3, maxSize: 15, nullable: false)
-        dateCreated(min: new Date(), nullable: false)
+        datecreated(min: new Date(), nullable: false)
     }
 }
