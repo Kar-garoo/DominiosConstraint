0d2b41125ad0e6f2242f9da05534318f6b79349a
Sergio Alejandro Diaz Pinilla <seradiazpin@unal.edu.co>
2015-04-14 23:03:50 -0500

    Relacion de Admin y Forum hasMany

--- grails-app/domain/dominiosconstraint/Admin.groovy
+++ grails-app/domain/dominiosconstraint/Admin.groovy
@@ -4,10 +4,14 @@ class Admin extends User{
 
     Integer level
     Double rating
+    Forum forums
+
+    static hasMany = Forum
 
 
     static constraints = {
         level(min: 1,max: 5,nullable: false)
         rating(ramge:0..100, nullable: false)
+        
     }
 }
