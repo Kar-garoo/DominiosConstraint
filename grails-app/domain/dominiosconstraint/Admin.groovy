60a2a5b7228b168a15110a9597f13ea01d0d7ab9
Daniel Salazar <jodsalazarmo@unal.edu.co>
2015-04-16 16:53:33 -0500

    Update Admin.groovy
    le hice un comentario Sergio

--- grails-app/domain/dominiosconstraint/Admin.groovy
+++ grails-app/domain/dominiosconstraint/Admin.groovy
@@ -8,7 +8,7 @@ class Admin extends User{
 
     static hasMany = Forum
 
-
+///Sergio le faltan las restrcciones del taller de Gorm, orden por nivel noo ?
     static constraints = {
         level(min: 1,max: 5,nullable: false)
         rating(ramge:0..100, nullable: false)
