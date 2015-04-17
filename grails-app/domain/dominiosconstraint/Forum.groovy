58298dd3905b2c7521a160502f09f950b04532a0
Daniel <jodsalazarmo@unal.edu.co>
2015-04-16 20:14:27 -0500

    agregando las relaciones para Forum y Regular

--- grails-app/domain/dominiosconstraint/Forum.groovy
+++ grails-app/domain/dominiosconstraint/Forum.groovy
@@ -6,6 +6,9 @@ class Forum {
     Date dateCreated
     String category
 
+    static belongsTo = [admin:Admin]
+    static hasMany = [post:Post]
+
     static constraints = {
         name(minSize: 3, maxSize: 20, unique: true, nullable: false)
         category(minSize: 3, maxSize: 15, nullable: false)
