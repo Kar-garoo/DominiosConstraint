e8c4727e19707b01832ddf4d40faec934edf1f1e
Daniel <jodsalazarmo@unal.edu.co>
2015-04-16 20:18:45 -0500

    Fecha actual en Forum. Pero falta manejarlo con Events

--- grails-app/domain/dominiosconstraint/Forum.groovy
+++ grails-app/domain/dominiosconstraint/Forum.groovy
@@ -12,7 +12,7 @@ class Forum {
     static constraints = {
         name(minSize: 3, maxSize: 20, unique: true, nullable: false)
         category(minSize: 3, maxSize: 15, nullable: false)
-        dateCreated(max: new Date(), nullable: false)
+        dateCreated(new Date(), nullable: false)
     }
 
 }
