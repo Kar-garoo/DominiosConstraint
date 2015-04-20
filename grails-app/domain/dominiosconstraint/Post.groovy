325ed26ef2a7fccd1a451e3308b89ba156bd0b84
JK <jucmendezco@unal.edu.co>
2015-04-20 15:32:23 -0500

    Post con las condiciones del taller Before (insert, update)

--- grails-app/domain/dominiosconstraint/Post.groovy
+++ grails-app/domain/dominiosconstraint/Post.groovy
@@ -5,7 +5,7 @@ class Post {
     String topic
     Date lastUpdate
     Boolean itsAllowed
-    Date datecreated
+    Date dateCreated
     Forum fatherForum_id
     Regular owner_id
     static belongsTo = [Forum, Regular]
@@ -15,6 +15,13 @@ class Post {
     static constraints = {
         topic(size: 3..50, blank: false, nullable: false)
         lastUpdate(min: new Date(), nullable: false)
-        datecreated(min:new Date(), nullable: false)
+        dateCreated(min:new Date())
+    }
+    def beforeInsert ={
+        dateCreated=new Date()
+        lastUpdate=new Date()
+    }
+    def beforeUpdate ={
+        lastUpdate=new Date()
     }
 }
