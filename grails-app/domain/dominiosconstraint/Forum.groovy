c0155321f3f77d98615f29d5fe3214c48f937a68
Daniel <jodsalazarmo@unal.edu.co>
2015-04-20 20:20:35 -0500

    Se termina con éxito todas las restricciones para la clase Forum. se hizo con Events. Usando beforeInsert

--- grails-app/domain/dominiosconstraint/Forum.groovy
+++ grails-app/domain/dominiosconstraint/Forum.groovy
@@ -2,9 +2,12 @@ package dominiosconstraint
 
 class Forum {
 
+
     String name
-    Date dateCreated
+    Date dateCreated = null
     String category
+
+    Admin admin
 
     static belongsTo = [admin:Admin]
     static hasMany = [post:Post]
@@ -12,7 +15,12 @@ class Forum {
     static constraints = {
         name(minSize: 3, maxSize: 20, unique: true, nullable: false)
         category(minSize: 3, maxSize: 15, nullable: false)
-        dateCreated(min: new Date(), nullable: false)
+        //dateCreated(min: new Date(), nullable: false)
     }
 
+    def beforeInsert() {
+        if (dateCreated == null) {
+            dateCreated = new Date()
+        }
+    }
 }
