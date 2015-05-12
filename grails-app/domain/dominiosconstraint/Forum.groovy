47a77fb0b65e87933f140aac61b40e3a2a1314c3
Sergio Alejandro Diaz Pinilla <seradiazpin@unal.edu.co>
2015-05-12 16:49:39 -0500

    Controller de forum Sin probar

--- grails-app/domain/dominiosconstraint/Forum.groovy
+++ grails-app/domain/dominiosconstraint/Forum.groovy
@@ -12,7 +12,15 @@ class Forum {
     static constraints = {
         name(minSize: 3, maxSize: 20, unique: true, nullable: false)
         category(minSize: 3, maxSize: 15, nullable: false)
-        dateCreated(min: new Date(), nullable: false)
+        dateCreated(nullable: false)
+    }
+
+    def beforeValidate() {
+        dateCreated = new Date();
+    }
+
+    String toString(){
+        "${name category}"
     }
 
 }
--- grails-app/domain/dominiosconstraint/User.groovy
+++ grails-app/domain/dominiosconstraint/User.groovy
@@ -8,11 +8,12 @@ class User {
     String password
 
     static constraints = {
+
         name(blank:false, minSize:3, maxSize:50)
         lastname(blank:false, size:3..50)
         age(nullable: false, min:13)
         username(blank:false, unique:true )
-        password(blank:false, minSize: 8, matches: /[a-z]+[A-Z]+[0-9]+/)
+        password(blank:false, minSize: 8, matches: /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/)
         //password(blank:false, minSize: 8, matches: /[[a-z]+[A-Z]+[0-9]+]+/)
     }
     static mapping = {
